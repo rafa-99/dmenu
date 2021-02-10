a7dbbadd23bab2d21734ef984941eb37be91cede
rafa_99 <rafa99@protonmail.com>
2021-02-10 17:01:45 +0000

    Updated Font To Fira Code

--- config.h
+++ config.h
@@ -6,7 +6,7 @@ static int centered = 0;                    /* -c option; centers dmenu on scree
 static int min_width = 500;                    /* minimum width when centered */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"JetBrains Mono Nerd Font:size=12"
+	"FiraCode Nerd Font:size=14"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 
